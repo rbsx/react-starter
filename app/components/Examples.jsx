6f7d31cbd264e14086f2a08a83115b6ae7be1388
rbsx <rbsxx@ya.ru>
2017-03-08 16:01:31 +0300

    About&Example pages ref

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,6 +3,7 @@ var React = require('react');
 const Examples = (props) => {
   return (
     <div>
+      <h3>Examples</h3>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit
       </p>
