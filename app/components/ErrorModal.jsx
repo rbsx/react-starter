4a409b27ac5b33c7cbda65bcb03c84d857579b3c
rbsx <rbsxx@ya.ru>
2017-03-11 23:03:55 +0300

    Final styles & popup fix

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,7 @@
 const React = require('react');
+const ReactDOM = require('react-dom');
+const ReactDOMServer = require('react-dom/server');
+
 
 const ErrorModal = React.createClass({
   getDefaultProps: function() {
@@ -11,12 +14,8 @@ const ErrorModal = React.createClass({
     message: React.PropTypes.string.isRequired
   },
   componentDidMount: function() {
-    let modal = new Foundation.Reveal($('#error-modal'));
-    modal.open();
-  },
-  render: function() {
     let {title, message} = this.props;
-    return (
+    let modalMarkup = (
       <div id="error-modal" className="reveal tiny text-center" data-reveal="">
         <h4>{title}</h4>
         <p>{message}</p>
@@ -27,6 +26,18 @@ const ErrorModal = React.createClass({
         </p>
       </div>
     );
+
+    let $modal = $(ReactDOMServer.renderToString(modalMarkup));
+    $(ReactDOM.findDOMNode(this)).html($modal);
+    
+    let modal = new Foundation.Reveal($('#error-modal'));
+    modal.open();
+  },
+  render: function() {
+    return (
+      <div>
+      </div>
+    )
   }
 });
 
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -2,9 +2,33 @@ const React = require('react');
 
 
 const WeatherMessage = ({location, temp}) => {
+
+  function tempBG(tempAsString) {
+    let t = +tempAsString;
+    console.log(tempAsString, t);
+    if (t < -10) {
+        return 'ice';
+    } else if ((t > -10) && (t < 0)) {
+        return 'cold';
+    } else if ((t > 0) && (t < 10)) {
+        return 'middle';
+    } else if ((t > 10) && (t < 20)) {
+        return 'warm';
+    } else if (t > 20) {
+        return 'hot';
+    }
+  }
+
+  let bgColor = tempBG(temp),
+      tempClass = `text-center ${bgColor}`;
+
+  console.log(bgColor, temp);
+
   return (
-    <div className="text-center">
-      this city is {location} and temperature {temp}
+    <div className="resulted-message">
+      <div className={tempClass}>
+        This is {temp} C* at <span className="capitalize">{location.toLowerCase()}</span>
+      </div>
     </div>
   );
 }
